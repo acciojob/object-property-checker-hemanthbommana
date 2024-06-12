3093d7ce2423eb80ca1b6f17569b69a569e12386
hemanthbommana <117904125+hemanthbommana@users.noreply.github.com>
2024-06-12 12:23:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	const isKeyPresent = sampleObject.hasOwnProperty(key);
+	return isKeyPresent;
+	
 }
 
 // Do not change the code below
